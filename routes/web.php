ec36c9f769e10702da73dbe99570116417dbcba6
Boris <bk@berweb.org>
2023-02-04 00:10:01 +0200

    Added package to work with url i18n

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,36 @@
 <?php
 
 use App\Http\Controllers;
-
-Route::get('/', [Controllers\PublicController::class, 'landing'])->name('public.landing');
-Route::get('/about', [Controllers\PublicController::class, 'about'])->name('public.about');
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::group([
-    'prefix' => '/blog'
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localizationRedirect', 'localeViewPath'],
 ], static function () {
-    Route::get('/', [Controllers\BlogController::class, 'postsList'])->name('blog.posts-list');
-});
+    Route::get('/', [Controllers\PublicController::class, 'landing'])->name('public.landing');
+    Route::get('/about', [Controllers\PublicController::class, 'about'])->name('public.about');
 
-Route::group([
-    'prefix' => '/auth',
-    'middleware' => 'guest',
-], static function () {
-    Route::get('/sign-in', [Controllers\AuthController::class, 'signIn'])->name('auth.sign-in');
-    Route::get('/sign-up', [Controllers\AuthController::class, 'signUp'])->name('auth.sign-up');
-});
+    Route::group([
+        'prefix' => '/blog'
+    ], static function () {
+        Route::get('/', [Controllers\BlogController::class, 'postsList'])->name('blog.posts-list');
+    });
 
-Route::group([
-    'prefix' => '/app',
-    'middleware' => 'auth',
-], static function () {
-    Route::get('/', Controllers\AppDashboardController::class)->name('app.dashboard');
-    Route::get('/profile', [Controllers\AppProfileController::class, 'view'])->name('app.profile-view');
-    Route::post('/profile/data', [Controllers\AppProfileController::class, 'saveUserData'])->name('app.profile-save-userdata');
-    Route::post('/profile/password', [Controllers\AppProfileController::class, 'updateUserPassword'])->name('app.profile-update-password');
+    Route::group([
+        'prefix' => '/auth',
+        'middleware' => 'guest',
+    ], static function () {
+        Route::get('/sign-in', [Controllers\AuthController::class, 'signIn'])->name('auth.sign-in');
+        Route::get('/sign-up', [Controllers\AuthController::class, 'signUp'])->name('auth.sign-up');
+    });
+
+    Route::group([
+        'prefix' => '/app',
+        'middleware' => 'auth',
+    ], static function () {
+        Route::get('/', Controllers\AppDashboardController::class)->name('app.dashboard');
+        Route::get('/profile', [Controllers\AppProfileController::class, 'view'])->name('app.profile-view');
+        Route::post('/profile/data', [Controllers\AppProfileController::class, 'saveUserData'])->name('app.profile-save-userdata');
+        Route::post('/profile/password', [Controllers\AppProfileController::class, 'updateUserPassword'])->name('app.profile-update-password');
+    });
 });
